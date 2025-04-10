3a681970ed8c672cf1c9ab9d23b31a06ba829157
miyavSevenRahip <emrr1261@gmail.com>
2025-04-10 11:10:53 +0300

    Düzelmezsen delircem base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Buraya kendi GitHub repo adını yaz
 export default defineConfig({
-  base: '/odeme-formu/',
+  base: '/odeme-formu/', 
   plugins: [react()],
 })
